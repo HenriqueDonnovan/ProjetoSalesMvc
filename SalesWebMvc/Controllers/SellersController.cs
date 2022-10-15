ed0e5d61428b5214e6a184773a4bcac3129d1017
Donnovan Henrique <donnovan@zylix.com.br>
2022-10-15 07:21:55 -0300

    Simple Create Form (Salvo)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -21,6 +22,15 @@ namespace SalesWebMvc.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
